90a12a163522dfc5680d8f055f2ff1b78e933fc1
Lopushka <sv13yat@gmail.com>
2022-12-17 09:35:36 +0300

    fin_v_1.0

---
+++ EX_1/Program.cs
@@ -0,0 +1,164 @@
+﻿
+
+
+/* // через массив
+static double Positivenamber(double[] myArray) // кол-во чисел > 0
+{
+    int counter = 0;
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        if (myArray[i] > 0)
+        {
+            counter++;
+        }
+
+    }
+    return counter;
+
+}
+
+static double Negativenamber(double[] myArray) // кол-во чисел < 0
+{
+    int counter = 0;
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        if (myArray[i] < 0)
+        {
+            counter++;
+        }
+
+    }
+    return counter;
+
+}
+
+
+
+
+
+starter1:
+Console.Write("Сколько целых положительных чисел будет введено: ");
+int namder = Convert.ToInt32(Console.ReadLine());
+
+if (namder > 0)
+{
+    double[] myArray = new double[namder];
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write($"\nВведите {i + 1} число: ");
+        myArray[i] = Convert.ToDouble(Console.ReadLine());
+
+    }
+
+    Console.WriteLine("\nВы ввели числа: ");
+
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        Console.Write(myArray[i] + ",");
+
+    }
+
+    Console.WriteLine();
+starter4:
+    Console.WriteLine("\nЧто хотите сделать?\n\n1 - Посчитать, сколько чисел больше 0\n\n2 - Посчитать, сколько чисел меньше 0\n\n3 - Найти максимальное\\минимальное число\n\n4 - Найти сумму чисел\n\n5 - Я просто люблю смотреть на свои ЦИфРЫ!!");
+
+    Console.WriteLine();
+    int namder_choice = Convert.ToInt32(Console.ReadLine());
+    if (namder_choice == 1)
+    {
+
+        Console.WriteLine("Чисел больше 0: " + Positivenamber(myArray));
+    }
+
+    Console.WriteLine();
+    if (namder_choice == 2)
+    {
+        Console.WriteLine("Чисел меньше 0: " + Negativenamber(myArray));
+    }
+
+    if (namder_choice == 3)
+    {
+        Console.WriteLine($"\nМаксимальное число:{myArray.Max()}\nМинимальное число:{myArray.Min()}");
+    }
+
+    if (namder_choice == 4)
+    {
+        Console.WriteLine($"Сумма чисел:{string.Format("{0:F5}", myArray.Sum())}"); // F1-(1) кол-во знаков после запятой
+    }
+
+    if (namder_choice == 5)
+    {
+        Console.WriteLine("Я знал что ты правильный человек!!! Наслаждайся!");
+        for (int i = 0; i < 100; i++)
+        {
+            Thread.Sleep(100);
+            for (int j = 0; j < myArray.Length; j++)
+            {
+                Console.Write(myArray[j]);
+                Thread.Sleep(50);
+            }
+
+        }
+        Console.WriteLine();
+        Console.WriteLine("\nСпасибо за внимание! ВСЕГО ХО-РО-ШЕГО!");
+
+    }
+
+    if (namder_choice > 5 || namder_choice < 1)
+    {
+        Console.Write("\nКоманда не распознана!\nПопытайтесь еще раз.");
+        goto starter4;
+    }
+}
+
+if (namder == 0)
+{
+    Console.Write($"\nПоздравляю вы решили вести {namder} чисел");
+starter2:
+    Console.Write($"\nЖелаете начать сначало?\n 1-Yes 2-No: ");
+    int choice = Convert.ToInt32(Console.ReadLine());
+
+    if (choice == 2)
+    {
+        Console.Write($"\nХорошего Вам дня! До новых встреч!");
+
+    }
+
+    if (choice == 1)
+    {
+        goto starter1;
+    }
+    if (choice > 2 || choice < 1)
+    {
+        Console.Write("\nКоманда не распознана!\nПопытайтесь еще раз.");
+        goto starter2;
+    }
+
+}
+
+if (namder < 0)
+{
+    Console.Write($"\nПоздравляю вы введи отрицательное число: {namder}");
+starter3:
+    Console.Write($"\nЖелаете начать сначало?\n 1-Yes 2-No: ");
+    int choice_1 = Convert.ToInt32(Console.ReadLine());
+
+    if (choice_1 == 2)
+    {
+        Console.Write($"\nХорошего Вам дня! До новых встреч!");
+
+    }
+
+    if (choice_1 == 1)
+    {
+        goto starter1;
+    }
+    if (choice_1 > 2 || choice_1 < 1)
+    {
+        Console.Write("\nКоманда не распознана!\nПопытайтесь еще раз.");
+        goto starter3;
+    }
+
+}
+*/
+
---
+++ EX_2/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Я могу найти пересение двух отрезков, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2");
+Console.WriteLine("\nНо значения b1, k1, b2 и k2 напиши мне сам!");
+Console.WriteLine($"И так начнем!");
+Console.WriteLine();
+Console.Write($"Введи b1= ");
+double numder_b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write($"Введи k1= ");
+double numder_k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write($"Введи b2= ");
+double numder_b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write($"Введи k2= ");
+double numder_k2 = Convert.ToDouble(Console.ReadLine());
+
+if (numder_k1==numder_k2 && numder_b1==numder_b2)
+{
+    Console.WriteLine($"Поскольку k1 ({numder_k1}) = K2 ({numder_k2}) и b1 ({numder_b1}) = b1 ({numder_b2}) прямые совпадают.");
+}
+
+if (numder_k1==numder_k2 && numder_b1!=numder_b2)
+{
+    Console.WriteLine($"Поскольку k1 ({numder_k1}) = k1 ({numder_k2}) прямые паралельны.");
+    
+}
+
+if (numder_k1!=numder_k2)
+{
+    double numder_x = (numder_b2 - numder_b1) / (numder_k1 - numder_k2);
+    double numder_y =  numder_k1 *numder_x + numder_b1;
+    Console.WriteLine($"Точка пересечения двух прямых имеет координаты:{numder_x} , {numder_y} ");
+    
+
+}
